76f8f60b423af8878a3a23572654bcd738baec1c
Budi Irawan <deerawan@gmail.com>
2016-06-08 00:01:03 +0700

    getRomeConfig as function

--- src/angular-rome.js
+++ src/angular-rome.js
@@ -27,47 +27,11 @@ var moment = require('moment');
 
     function link(scope, element, attrs, ngModelCtrl) {
       var inputElement = element.find('input');
-      var input = element.find('input')[0];
+      var input = inputElement[0];
 
       addNonDirectiveAttributes(attrs, element, inputElement);
 
-      var romeConfig = {};
-
-      if (attrs.initialValue) { romeConfig.initialValue = attrs.initialValue; }
-      if (attrs.time) { romeConfig.time = attrs.time === 'true'; }
-      if (attrs.date) { romeConfig.date = attrs.date === 'true'; }
-      if (attrs.inputFormat) { romeConfig.inputFormat = attrs.inputFormat; }
-      if (attrs.weekStart) { romeConfig.weekStart = Number(attrs.weekStart); }
-      if (attrs.monthsInCalendar) { romeConfig.monthsInCalendar = Number(attrs.monthsInCalendar); }
-      if (attrs.min) { romeConfig.min = attrs.min; }
-      if (attrs.max) { romeConfig.max = attrs.max; }
-      if (attrs.dayFormat) { romeConfig.dayFormat = attrs.dayFormat; }
-      if (attrs.weekdayFormat) { romeConfig.weekdayFormat = attrs.weekdayFormat; }
-      if (attrs.styles) { romeConfig.styles = JSON.parse(attrs.styles); }
-      if (attrs.timeInterval) { romeConfig.timeInterval = Number(attrs.timeInterval); }
-      if (attrs.autoHideOnBlur) { romeConfig.autoHideOnBlur = attrs.autoHideOnBlur === 'true'; }
-      if (attrs.autoClose) { romeConfig.autoClose = attrs.autoClose === 'true'; }
-
-      if (attrs.beforeEq) {
-        var beforeElement = document.getElementById(attrs.beforeEq);
-        romeConfig.dateValidator = rome.val.beforeEq(beforeElement);
-      }
-
-      if (attrs.before) {
-        var beforeElement = document.getElementById(attrs.before);
-        romeConfig.dateValidator = rome.val.after(beforeElement);
-      }
-
-      if (attrs.afterEq) {
-        var afterElement = document.getElementById(attrs.afterEq);
-        romeConfig.dateValidator = rome.val.afterEq(afterElement);
-      }
-
-      if (attrs.after) {
-        var afterElement = document.getElementById(attrs.after);
-        romeConfig.dateValidator = rome.val.after(afterElement);
-      }
-
+      var romeConfig = getRomeConfig(attrs);
       var romeElement = rome(input, romeConfig);
 
       ngModelCtrl.$formatters.push(function(modelValue) {
@@ -86,24 +50,70 @@ var moment = require('moment');
       });
 
       romeElement.on('data', function (value) {
+        input.value = value;
         scope.$apply(function () {
           ngModelCtrl.$setViewValue({
             date: value
           });
-          input.value = value;
         });
       });
+    }
 
-      function addNonDirectiveAttributes(attrs, directiveElement, inputElement) {
-        angular.forEach({
-          'id': attrs.id
-        }, function (value, name) {
-          if (angular.isDefined(value)) {
-            directiveElement.removeAttr(name);
-            inputElement.attr(name, value);
-          }
-        });
+    function addNonDirectiveAttributes(attrs, directiveElement, inputElement) {
+      angular.forEach({
+        'id': attrs.id
+      }, function (value, name) {
+        if (angular.isDefined(value)) {
+          directiveElement.removeAttr(name);
+          inputElement.attr(name, value);
+        }
+      });
+    }
+
+    /**
+     * Get rome configuration
+     *
+     * @param {Object} attrs - directive element attributes
+     */
+    function getRomeConfig(attrs) {
+      var romeConfig = {};
+
+      if (attrs.initialValue) { romeConfig.initialValue = attrs.initialValue; }
+      if (attrs.time) { romeConfig.time = attrs.time === 'true'; }
+      if (attrs.date) { romeConfig.date = attrs.date === 'true'; }
+      if (attrs.inputFormat) { romeConfig.inputFormat = attrs.inputFormat; }
+      if (attrs.weekStart) { romeConfig.weekStart = Number(attrs.weekStart); }
+      if (attrs.monthsInCalendar) { romeConfig.monthsInCalendar = Number(attrs.monthsInCalendar); }
+      if (attrs.min) { romeConfig.min = attrs.min; }
+      if (attrs.max) { romeConfig.max = attrs.max; }
+      if (attrs.dayFormat) { romeConfig.dayFormat = attrs.dayFormat; }
+      if (attrs.weekdayFormat) { romeConfig.weekdayFormat = attrs.weekdayFormat; }
+      if (attrs.styles) { romeConfig.styles = JSON.parse(attrs.styles); }
+      if (attrs.timeInterval) { romeConfig.timeInterval = Number(attrs.timeInterval); }
+      if (attrs.autoHideOnBlur) { romeConfig.autoHideOnBlur = attrs.autoHideOnBlur === 'true'; }
+      if (attrs.autoClose) { romeConfig.autoClose = attrs.autoClose === 'true'; }
+
+      if (attrs.beforeEq) {
+        var beforeElementEq = document.getElementById(attrs.beforeEq);
+        romeConfig.dateValidator = rome.val.beforeEq(beforeElementEq);
       }
+
+      if (attrs.before) {
+        var beforeElement = document.getElementById(attrs.before);
+        romeConfig.dateValidator = rome.val.after(beforeElement);
+      }
+
+      if (attrs.afterEq) {
+        var afterElementEq = document.getElementById(attrs.afterEq);
+        romeConfig.dateValidator = rome.val.afterEq(afterElementEq);
+      }
+
+      if (attrs.after) {
+        var afterElement = document.getElementById(attrs.after);
+        romeConfig.dateValidator = rome.val.after(afterElement);
+      }
+
+      return romeConfig;
     }
   }
 })();
